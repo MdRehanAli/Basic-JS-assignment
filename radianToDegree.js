2f87dd797900654e85b6216dbbc211148c40ac61
MdRehanAli <rehanali21d>
2023-03-03 01:25:06 +0600

    radianToDegree problem solved

---
+++ radianToDegree.js
@@ -0,0 +1,19 @@
+function radianToDegree(radian) {
+    if (typeof radian !== 'number') {
+        return 'Please Enter a number!';
+    }
+
+    const pi = Math.PI;
+
+    // calculation
+    const degree = (180 / pi) * radian;
+
+    // calculated value in round figure
+    const degreeInRound = Math.round(degree);
+    return degreeInRound;
+
+}
+
+const radian = 5;
+const degrees = radianToDegree(radian);
+console.log(degrees);
